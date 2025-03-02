0c4fe2889241da5030a89f680b430a5b6c31a309
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-03-02 23:05:21 +0300

    Add expression function

--- 2/prog.c
+++ 2/prog.c
@@ -13,6 +13,21 @@ int is_operator(char data){
 	return (strchr("+-*/", data) != NULL);
 }
 
+char *expression(char *operand1, char *operand2, char operator){
+	char *res = calloc(strlen(operand1) + strlen(operand2) + 8, sizeof(char));
+	res[0] = '(';
+	res[1] = ' ';
+	memcpy(res + 2, operand2, strlen(operand2) * sizeof(char));
+	res[2 + strlen(operand2)] = ' ';
+	res[3 + strlen(operand2)] = operator;
+	res[4 + strlen(operand2)] = ' ';
+	memcpy(res + 5 + strlen(operand2), operand1, strlen(operand1) * sizeof(char));
+	res[5 + strlen(operand2) + strlen(operand2)] = ' ';
+	res[6 + strlen(operand1) + strlen(operand2)] = ')';
+	free(operand1);
+	free(operand2);
+	return res;
+}
 
 int main(){
 	Stack *stack = stack_new();
@@ -47,14 +62,10 @@ int main(){
 			free(operand1);
 			break;
 		}
-		char *res = calloc(strlen(operand1) + strlen(operand2) + 4, sizeof(char));
-		res[0] = '(';
-		memcpy(res + 1, operand2, strlen(operand2) * sizeof(char));
-		res[1 + strlen(operand2)] = data;
-		memcpy(res + 2 + strlen(operand2), operand1, strlen(operand1) * sizeof(char));
-		res[2 + strlen(operand1) + strlen(operand2)] = ')';
-		free(operand1);
-		free(operand2);
+		char *res = expression(operand1, operand2, data);
+		if(!res){
+			break;
+		}
 		flag = push(stack, res);
 		if(flag != ERR_OK){
 			break;

c8881f26e74d09f1c335600b2f22506fc84dd0eb
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-03-19 15:26:12 +0300

    Add dialog_table.h

---
+++ 3a/dialog_table.h
@@ -0,0 +1,16 @@
+#ifndef DIALOG_TABLE_H
+#define DIALOG_TABLE_H
+
+#include "err.h"
+#include "table.h"
+
+err D_create(Table *);
+err D_insert(Table *);
+err D_delete(Table *);
+err D_show(Table *);
+err D_input(Table *);
+err D_reorganize(Table *);
+err D_find(Table *);
+err D_clear(Table *);
+
+#endif

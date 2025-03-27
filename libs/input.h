4ae794c07853eb83dae7091fd91283578cda7f5a
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-03-27 11:54:12 +0300

    Add txt_input_int

--- libs/input.h
+++ libs/input.h
@@ -5,6 +5,7 @@
 
 err input_uint(unsigned*, unsigned, unsigned);
 err txt_input_int(FILE*, int *, int, int);
+err txt_input_uint(FILE*, unsigned *, unsigned, unsigned);
 err bin_input_int(FILE*, int *, int, int);
 
 #endif

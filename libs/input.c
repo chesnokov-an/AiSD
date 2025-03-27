923b46099d689fc5dab9d4768e7dd434240bd2f8
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-03-27 11:11:10 +0300

    Add readline lib and input_int from bin and txt

--- libs/input.c
+++ libs/input.c
@@ -17,3 +17,26 @@ err input_uint(unsigned *data, unsigned start, unsigned end){
     }
     return ERR_OK;
 }
+
+err txt_input_int(FILE* file_name, int *data, int start, int end){
+    char end_of_scanf = ' ';
+    int flag_scanf = fscanf(file_name, "%9d%c", data, &end_of_scanf);
+	if(flag_scanf == EOF){
+		return ERR_EOF;
+	}
+	if((flag_scanf != 2) || (end_of_scanf != '\n') || (*data < start) || (*data > end)){
+		return ERR_VAL;
+	}
+    return ERR_OK;
+}
+
+err bin_input_int(FILE* file_name, int *data, int start, int end){
+	fread(data, sizeof(int), 1, file_name);
+	if(feof(file_name)){
+		return ERR_EOF;
+	}
+	if((ferror(file_name)) || (*data < start) || (*data > end)){
+		return ERR_VAL;
+	}
+	return ERR_OK;
+}

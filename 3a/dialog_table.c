1509becac0515a142ffa2f9d233fac713546da63
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-03-29 15:19:38 +0300

    Add dialog and dialog_functions

--- 3a/dialog_table.c
+++ 3a/dialog_table.c
@@ -9,6 +9,59 @@
 #include "dialog_table.h"
 #include "my_readline.h"
 
+void message(err flag){
+	switch(flag){
+	case ERR_MEM:
+		printf("\nОшибка при выделении памяти.\n");
+		break;
+	case ERR_VAL:
+		printf("\nНекорректное значение.\n");
+		break;
+	case ERR_NULL:
+		printf("\nПередан нулевой указатель.\n");
+		break;
+	case ERR_FULL:
+		printf("\nТаблица заполнена.\n");
+		break;
+	case ERR_EMPTY:
+		printf("\nТаблица пуста.\n");
+		break;
+	case ERR_EOF:
+		printf("\nЗавершение программы.\n");
+		break;
+	case ERR_OK:
+		break;
+	}
+}
+
+void Dialog(){
+	err (*func_array[])(Table *) = {D_input, D_load, D_insert, D_delete, D_find, D_show, D_reorganize};
+
+	unsigned msize = 0;
+	printf("Введите количество ячеек памяти, которые нужно выделить под таблицу: ");
+	err flag = input_uint(&msize, 0, UINT_MAX);
+	if(flag == ERR_EOF){ goto end_program; }
+	Table *table = create_table(msize);
+	int option = -1;
+	do{
+		printf("\n\n---------- МЕНЮ ----------\nВозможные команды:\n\n");
+		printf("0: Завершение программы.\n1: Ввод таблицы с клавиатуры.\n2: Импорт таблицы из файла.\n3: Вставка нового элемента.\n4: Удаление элемента.\n5: Поиск элемента.\n6: Вывод содержимого массива.\n7: Реорганизация таблицы.\n\n");
+		printf("Выберите опцию: ");
+		flag = input_int(&option, 0, 7);
+		printf("\n");
+		if(flag == ERR_EOF || option == 0){ goto end_program; }
+		flag = func_array[option-1](table);
+
+		if(flag != ERR_OK){ message(flag); }
+	}while(option != 0);
+	goto end_program;
+
+end_program:
+	clear_table(table);
+	free(table);
+	return;
+}
+
 err D_insert(Table *table){
 	if(table == NULL){
 		return ERR_NULL;
@@ -94,8 +147,18 @@ clean_and_return:
 	return flag;
 }
 
+err D_load(Table *table){
+	char *str_file = readline("Введите название файла: ");
+	FILE *file = fopen(str_file, "r");
+	free(str_file);
+	err flag = load_from_txt(table, file);
+	fclose(file);
+	return flag;
+}
+
 err D_reorganize(Table *table){
 	err flag = reorganize_table(table);
+	show_table(table);
 	return flag;
 }
 
@@ -126,9 +189,5 @@ err D_find(Table *table){
 		clear_table(new_table);
 		free(new_table);
 	}
-	return ERR_OK;
-}
-err D_clear(Table *table){
-	clear_table(table);
 	return ERR_OK;
 }

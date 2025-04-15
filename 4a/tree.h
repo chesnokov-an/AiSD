7a36c9c26b7f2aa1bce1daa2671e8bbe6fc52a29
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-04-15 21:56:23 +0300

    Start delete

--- 4a/tree.h
+++ 4a/tree.h
@@ -14,8 +14,13 @@ typedef struct Tree{
 	Node *root;
 }Tree;
 
-Node *find(const Tree * const, const keytype const);
 err insert_elem(Tree * const, const keytype const, const infotype);
 err delete_elem(Tree * const, const keytype const);
+void traversal(const Tree * const);
+Node *find(const Tree * const, const keytype const);
+Node *spec_find(const Tree * const, const keytype const);
+void format_tree(const Tree * const);
+void graph_tree(const Tree * const);
+void import_tree(const Tree * const, FILE *);
 
 #endif

50586127f9ae05b3048a89b7748aa47c9996e438
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-04-21 21:31:50 +0300

    Add next and prev to insert

--- 4a/tree.c
+++ 4a/tree.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tree.h"
 
 #define left children[0]
 #define right children[1]
+#define prev children[2]
+#define next children[3]
 
 Tree *create_tree(){
 	Tree *tree = (Tree *)calloc(1, sizeof(Tree));
 	return tree;
 }
 
-Node *create_node(const keytype const key, const infotype info){
+Node *create_node(const char * const key, const unsigned info){
 	Node *node = (Node *)calloc(1, sizeof(Node));
 	if(node == NULL){ return NULL; }
 	node->children = (Node **)calloc(1, sizeof(Node *));
@@ -18,7 +21,7 @@ Node *create_node(const keytype const key, const infotype info){
 		free(node);
 		return NULL;
 	}
-	node->info = (infotype *)calloc(1, sizeof(infotype));
+	node->info = (unsigned *)calloc(1, sizeof(unsigned));
 	if(node->info == NULL){
 		free(node->children);
 		free(node);
@@ -26,6 +29,7 @@ Node *create_node(const keytype const key, const infotype info){
 	}
 	node->key = strdup(key);
 	*(node->info) = info;
+	node->is_thread = 0;
 	return node;
 }
 
@@ -37,11 +41,11 @@ void clear_node(Node *node){
 	free(node);
 }
 
-char cmp(keytype key1, keytype key2){
+char cmp(const char * const key1, const char * const key2){
 	return strcmp(key1, key2);
 }
 
-Node *find(const Tree * const tree, const keytype const key){
+Node *find(const Tree * const tree, const char * const key){
 	Node *node = tree->root;
 	char cmp_val = 0;
 	while(node != NULL){
@@ -59,11 +63,12 @@ Node *find(const Tree * const tree, const keytype const key){
 	return node;
 }
 
-err insert_elem(Tree * const tree, const keytype const key, const infotype info){
+err insert_elem(Tree * const tree, const char * const key, const unsigned info){
 	if(tree == NULL){ return ERR_NULL; }
 	if(tree->root == NULL){
 		tree->root = create_node(key, info);
 		if(tree->root == NULL){ return ERR_MEM; }
+		return ERR_OK;
 	}
 	Node *node = tree->root;
 	Node *pre_node = NULL;
@@ -84,23 +89,27 @@ err insert_elem(Tree * const tree, const keytype const key, const infotype info)
 	if(cmp_val > 0){
 		pre_node->left = create_node(key, info);
 		if(pre_node->left == NULL){ return ERR_MEM; }
+		pre_node->left->next = pre_node->next;
+		pre_node->left->prev = pre_node;
+		if(pre_node->next != NULL){
+			pre_node->next->prev = pre_node->left;
+		}
+		pre_node->next = pre_node->left;
 	}
 	else{
 		pre_node->right = create_node(key, info);
 		if(pre_node->right == NULL){ return ERR_MEM; }
+		pre_node->right->next = pre_node;
+		pre_node->right->prev = pre_node->prev;
+		if(pre_node->prev != NULL){
+			pre_node->prev->next = pre_node->right;
+		}
+		pre_node->prev = pre_node->right;
 	}
-	firmware(tree);
 	return ERR_OK;
 }
 
-char is_possible_to_delete(const Node * const node){
-	if(node->left == NULL || node->right){
-		return 1;
-	}
-	return 0;
-}
-
-err delete_elem(Tree * const tree, const keytype const key){
+err delete_elem(Tree * const tree, const char * const key){
 	if(tree == NULL){ return ERR_NULL; }
 	if(tree->root == NULL){
 		return ERR_EMPTY;
@@ -131,7 +140,7 @@ err delete_elem(Tree * const tree, const keytype const key){
 	// Удаляем, если нет потомков
 	if((node->left == NULL) && (node->right == NULL)){
 		if(left_flag == -1){ tree->root = NULL; }
-		goto end_of_insert;
+		goto end_of_delete;
 		return ERR_OK;
 	}
 	// Удаляем, если один потомок
@@ -145,7 +154,7 @@ err delete_elem(Tree * const tree, const keytype const key){
 		else{
 			pre_node->right = node->right;
 		}
-		goto end_of_insert;
+		goto end_of_delete;
 		return ERR_OK;
 	}
 	if(node->right == NULL){
@@ -158,7 +167,7 @@ err delete_elem(Tree * const tree, const keytype const key){
 		else{
 			pre_node->right = node->left;
 		}
-		goto end_of_insert;
+		goto end_of_delete;
 		return ERR_OK;
 	}
 	// Удаляем, если два потомка
@@ -179,7 +188,7 @@ err delete_elem(Tree * const tree, const keytype const key){
 			pre_node->right = min_right;
 		}
 		min_right->left = node->left;
-		goto end_of_insert;
+		goto end_of_delete;
 		return ERR_OK;
 	}
 	if(left_flag == -1){
@@ -194,26 +203,22 @@ err delete_elem(Tree * const tree, const keytype const key){
 	pre_min->left = min_right->right;
 	min_right->right = node->right;
 	min_right->left = node->left;
-	goto end_of_insert;
+	goto end_of_delete;
 
-end_of_insert:
+end_of_delete:
 	clear_node(node);
 	free(node);
-	firmware(tree);
 	return ERR_OK;
 }
 
-void firmware(Tree * const tree) {
-	Node *prev = NULL;
-	firmware_by_node(tree->root, prev);
-}
-
-void firmware_by_node(Node * const node, Node * const prev){
-	if(node == NULL){ return; }
-
-	firmware_by_node(node->left, prev);
-	firmware_by_node(node->right, prev);
-
-	node->next = prev;
-	prev = node;
+void traversal(const Tree * const tree){
+	if(tree == NULL){ return; }
+	Node *node = tree->root;
+	while(node->right != NULL){
+		node = node->right;
+	}
+	while(node != NULL){
+		printf("%s %u\n", node->key, *node->info);
+		node = node->next;
+	}
 }

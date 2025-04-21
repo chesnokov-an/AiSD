d224f19a6bf8417810badc74fb8855673d82ec1e
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-04-21 21:31:50 +0300

    Add next and prev to insert

--- 4a/tree.h
+++ 4a/tree.h
@@ -1,27 +1,27 @@
 #ifndef TREE_H
 #define TREE_H
 
-typedef char* keytype;
-typedef unsigned infotype;
+#include "err.h"
 
 typedef struct Node{
-	keytype key;
+	char *key;
 	struct Node **children;
-	infotype *info;
+	unsigned *info;
+	char is_thread;
 }Node;
 
 typedef struct Tree{
 	Node *root;
 }Tree;
 
-err insert_elem(Tree * const, const keytype const, const infotype);
-err delete_elem(Tree * const, const keytype const);
-void traversal(const Tree * const);
-Node *find(const Tree * const, const keytype const);
-Node *spec_find(const Tree * const, const keytype const);
-void format_tree(const Tree * const);
-void graph_tree(const Tree * const);
-void import_tree(const Tree * const, FILE *);
+err insert_elem(Tree * const, const char * const, const unsigned);
+err delete_elem(Tree * const, const char * const);
 void firmware(Tree * const tree);
+void traversal(const Tree * const);
+Node *find(const Tree * const, const char * const);
+Node *spec_find(const Tree * const, const char * const);
+void show(const Tree * const);
+void graph(const Tree * const);
+void import_tree(const Tree * const, FILE *);
 
 #endif

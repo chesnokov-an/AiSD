64139a8f2322ada335cb24d06bb74c63fa3e76b4
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-05-06 19:21:13 +0300

    Finished timing

--- 4a/timing/generate.h
+++
@@ -1,9 +0,0 @@
-#ifndef GENERATE_H
-#define GENERATE_H
-
-#include "tree.h"
-
-char *generate_key();
-Tree *generate_tree(int key, int size);
-
-#endif
--- 4a/timing/timer.c
+++ 4a/timing/timer.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include "err.h"
 #include "my_readline.h"
-#include "generate.h"
 #include "tree.h"
 
 int main(int argc, char **argv){
@@ -18,55 +17,100 @@ int main(int argc, char **argv){
 	int size = atoi(argv[1]);
 	int iters = atoi(argv[2]);
 
-	FILE *file = fopen("russian_nouns.txt", "r");
-	char **keys = (char **)calloc(51301, sizeof(char *));
-	for(int i = 0; i < 51301; i++){
+	FILE *file = fopen("words_alpha.txt", "r");
+	char **keys = (char **)calloc(370105, sizeof(char *));
+	for(int i = 0; i < 370105; i++){
 		keys[i] = txt_readline(file);
 	}
 	fclose(file);
 
-	Tree *tree = create_tree();
+	Tree **data = (Tree **)calloc(iters, sizeof(Tree *));
 	unsigned info = 0;
-	char *key = NULL;
-	for(int i = 0; i < size; i++){
-		key = keys[rand() % 51301];
-		info = rand() % UINT_MAX;
-		insert_elem(tree, key, info);
+	char ***cur_keys = (char ***)calloc(iters, sizeof(char **));
+
+	for(int j = 0; j < iters; j++){
+		data[j] = create_tree();
+		cur_keys[j] = (char **)calloc(size, sizeof(char *));
+		for(int i = 0; i < size; i++){
+			cur_keys[j][i] = keys[rand() % 370105];
+			info = rand() % UINT_MAX;
+			insert_elem(data[j], cur_keys[j][i], info);
+		}
 	}
 
 	// вставка
 	struct timeval  tv1, tv2;
 	gettimeofday(&tv1, NULL);
-	for(int i = 0; i < iters; i++){
-		key = keys[rand() % 51301];
-		info = rand() % UINT_MAX;
-		insert_elem(tree, key, info);
-
+	char *key = NULL;
+	for(int j = 0; j < iters; j++){
+		for(int i = 0; i < 1000; i++){
+			if(i%2 == 0){
+				key = keys[rand() % 370105];
+			}
+			else{
+				key = cur_keys[j][rand() % size];
+			}
+			info = rand() % UINT_MAX;
+			insert_elem(data[j], key, info);
+		}
 	}
 	gettimeofday(&tv2, NULL);
 	printf("Усреднённое за %d итераций время ВСТАВКИ %d элементов в дерево составило %lf секунд.\n", iters, size, (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
 
 	// поиск
 	gettimeofday(&tv1, NULL);
-	for(int i = 0; i < iters; i++){
-		key = keys[rand() % 51301];
-		find(tree, key);
+	for(int j = 0; j < iters; j++){
+		for(int i = 0; i < 1000; i++){
+			if(i%2 == 0){
+				key = keys[rand() % 370105];
+			}
+			else{
+				key = cur_keys[j][rand() % size];
+			}
+			find(data[j], key);
+		}
 	}
 	gettimeofday(&tv2, NULL);
 	printf("Усреднённое за %d итераций время ПОИСКА в дереве из %d элементов составило %lf секунд.\n", iters, size, (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
 
 	// Удаление
 	gettimeofday(&tv1, NULL);
-	for(int i = 0; i < iters; i++){
-		key = keys[rand() % 51301];
-		delete_elem(tree, key);
+	for(int j = 0; j < iters; j++){
+		for(int i = 0; i < 1000; i++){
+			if(i%2 == 0){
+				key = keys[rand() % 370105];
+			}
+			else{
+				key = cur_keys[j][rand() % size];
+			}
+			delete_elem(data[j], key);
+		}
 	}
 	gettimeofday(&tv2, NULL);
 	printf("Усреднённое за %d итераций время УДАЛЕНИЯ из деревв из %d элементов составило %lf секунд.\n", iters, size, (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
 
-	clear_tree(tree);
-	free(tree);
-	for(int i = 0; i < 51301; i++){
+
+	struct timeval  tv1, tv2;
+	gettimeofday(&tv1, NULL);
+	char *key = NULL;
+	for(int j = 0; j < iters; j++){
+		traversal(data[j]);
+	}
+	gettimeofday(&tv2, NULL);
+	printf("Усреднённое за %d итераций время ОБХОДА дерева составило %lf секунд.\n", iters, size, (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec));
+
+
+	for(int j = 0; j < iters; j++){
+		clear_tree(data[j]);
+		free(data[j]);
+	}
+	free(data);
+	for(int j = 0; j < iters; j++){
+		free(cur_keys[j]);
+	}
+
+	free(cur_keys);
+	for(int i = 0; i < 370105; i++){
 		free(keys[i]);
 	}
 	free(keys);

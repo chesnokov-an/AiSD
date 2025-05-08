befba9445b48d95c0d2202af4e46f8b5ac467564
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-05-08 18:00:20 +0300

    Add functions to counter

--- 4a/extra_task/counter.c
+++ 4a/extra_task/counter.c
@@ -7,33 +7,43 @@
 #include "my_readline.h"
 #include "err.h"
 
+void prepare_line(char * const line){
+	for(size_t i = 0; i < strlen(line); i++){
+		if(ispunct(line[i])){
+			line[i] = ' ';
+		}
+		line[i] = tolower(line[i]);
+	}
+}
+
+void add_word(Tree * const tree, char *word){
+	Node *node = find(tree, word);
+	if(node == NULL){
+		insert_elem(tree, word, 0);
+		node = find(tree, word);
+	}
+	*node->info += 1;
+}
+
 int main(){
 	Tree *tree = create_tree();
 	FILE *file = input_correct_file();
 	while(!file){
 		file = input_correct_file();
 	}
+
 	char *line = txt_readline(file);
 	while(line != NULL){
-		for(size_t i = 0; i < strlen(line); i++){
-			if(ispunct(line[i])){
-				line[i] = ' ';
-			}
-			line[i] = tolower(line[i]);
-		}
+		prepare_line(line);
 		char *word = strtok(line, "\t ");
 		while(word != NULL){
-			Node *node = find(tree, word);
-			if(node == NULL){
-				insert_elem(tree, word, 0);
-				node = find(tree, word);
-			}
-			*node->info += 1;
+			add_word(tree, word);
 			word = strtok(NULL, "\t ");
 		}
 		free(line);
 		line = txt_readline(file);
 	}
+
 	fclose(file);
 	file = fopen("res.txt", "w");
 	write_to_txt(tree, file);
